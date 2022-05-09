df86d027724a083f17eb983278cb140a3f061d9c
Al-amin Sheikh <azalamins@gmail.com>
2022-05-10 05:55:54 +0600

    navbar done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Login = () => {
     return (
         <div>
-            <h1>Welcome to Home</h1>
+            <h1>Welcome to login</h1>
         </div>
     );
 };
 
-export default Home;
+export default Login;
